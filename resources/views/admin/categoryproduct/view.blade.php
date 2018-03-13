c3c30e3ef3b29dcfe30f71900f8b6d2ab00571e2
sroun <sron.oem@gmail.com>
2018-03-13 18:52:49 +0700

    CK-editor

--- resources/views/admin/categoryproduct/view.blade.php
+++ resources/views/admin/categoryproduct/view.blade.php
@@ -20,7 +20,13 @@
             <td >{{$cat->pivot->name}}</td>
             <td >{{$cat->parent ? DB::table('categoryproduct_language')->where([['language_id',$categoryproduct->id],['categoryproduct_id',$cat->parent]])->value('name') : ''}}</td>
             <td >{{$cat->user->name}}</td>
-            <td class="center">{{$cat->publish ? trans('label.yes') : trans('label.no')}}</td>
+            <td class="center">
+                @if($cat->publish)
+                    <i class="fa fa-globe text-info" title="publish" aria-hidden="true"></i>
+                @else
+                    <i class="fa fa-lock text-warning" title="private" aria-hidden="true"></i>
+                @endif
+            </td>
             <td class="center">
                 <a class="cursor-pointer padding-7px"​​ onclick='editCatPro("{{$cat->id}}","{{$cat->pivot->language_id}}")' data-toggle="modal" data-target=".bs-example-modal-sm"><i class="fa fa-edit"></i></a>
                 <a class="cursor-pointer padding-7px text-danger" onclick='deleteCatpro("{{$cat->id}}")'><i class="fa fa-trash"></i></a>
--- resources/views/admin/product/create.blade.php
+++ resources/views/admin/product/create.blade.php
@@ -1,8 +1,22 @@
 @extends('admin.master')
 
 @section('content')
+    <br>
+    <div class="container-fluid">
+        <div class="panel panel-default">
+            <div class="panel-heading {{Lang::locale()==='kh' ? 'kh-moul' : 'time-roman'}}">
+                {{trans('label.product')}}
+            </div>
+            <div class="panel-body">
+                <div class="row">
 
+                </div>
+            </div>
+            <div class="panel-footer">
 
+            </div>
+        </div>
+    </div>
 @endsection
 
 
